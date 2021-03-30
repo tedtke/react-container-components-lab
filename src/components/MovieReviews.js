6e8f94e4396e0bcbc46d316eafa90074fdac1485
Teodoro Garcia <tedtke514@gmail.com>
2021-03-30 17:13:38 -0500

    commit tree 92b982a0a0eb07c56eb5ff5f250f30b1688f8b96

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,25 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const Review = ({
+  headline, byLine, link, summary_short
+}) => {
+  return (
+    <div key={headline} className="review">
+      <header>
+        <a className="review-link" href={link.url}>{headline}</a>
+        <br/>
+        <span className="author">{byLine}</span>
+      </header>
+      <blockquote>{summary_short}</blockquote>
+    </div>
+  );
+};
+
+const MovieReviews = ({reviews}) => <div className="review-list">{reviews.map(Review)}</div>;
+
+MovieReviews.defaultprops = {
+  reviews: []
+};
+
+export default MovieReviews;
